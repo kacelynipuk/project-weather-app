46ac9e2c554a1dfe1db26380c057f5d464cf7091
kacelynip <kacelynipuk@gmail.com>
2023-02-09 21:13:13 +0000

    Update the time & date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,41 @@
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let minute = now.getMinutes(); // 0,1,2, 12
+  let hour = now.getHours(); //1, 2, 3, 4
+  let day = days[now.getDay()]; //1, 2, 3, 4
+  let date = now.getDate();
+  let month = months[now.getMonth()];
+  let year = now.getFullYear();
+
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
+
+  return `${date} ${month} ${year} (${day}) ${hour}:${minute}`;
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
@@ -13,6 +51,8 @@ function displayTemperature(response) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000); //to call function for calculating time
 }
 let apiKey = "2f63a97f4at8dac8o3e80889b0365010";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=london&key=${apiKey}&units=metric`;
